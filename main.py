bf66416b3cfbb008122950821d641d46a84f20d5
pulkit-yadav-aptos-com <pulkit.yadav@aptos.com>
2026-04-19 11:21:31 +0530

    hello-world chain

--- main.py
+++ main.py
@@ -1,10 +1,49 @@
 from dotenv import load_dotenv
+from langchain_ollama import ChatOllama
+from langchain_core.prompts import PromptTemplate
 
 load_dotenv()
 
 
 def main():
     print("Hello from langchain-course!")
+    information = """
+    Mohandas Karamchand Gandhi (/ˈɡɑːndi, ˈɡændi/;[1] GAHN-dee; 2 October 1869 – 30 January 1948) was an Indian lawyer,[2] anti-colonial nationalist[3] and political ethicist[4] who employed nonviolent resistance to lead the successful campaign for India's independence from British rule,[5] and to later inspire movements for civil rights and freedom across the world. The honorific Mahātmā (Sanskrit: "great-souled", "venerable"), first applied to him in 1914 in South Africa, is now used throughout the world.[6][7]
+
+Born and raised in a Hindu family in coastal Gujarat, Gandhi was trained in the law at the Inner Temple in London and was called to the bar at the age of 22. After two uncertain years in India, where he was unable to start a successful law practice, Gandhi moved to South Africa in 1893 to represent an Indian merchant in a lawsuit. He went on to live in South Africa for the next 21 years. Here, Gandhi raised a family and first employed nonviolent resistance in a campaign for civil rights. In 1915, aged 45, he returned to India and soon set about organising peasants, farmers, and urban labourers to protest against discrimination and excessive land tax.
+
+Assuming leadership of the Indian National Congress in 1921, Gandhi led nationwide campaigns for easing poverty, expanding women's rights, building religious and ethnic amity, ending untouchability, and, above all, achieving swaraj or self-rule. Gandhi adopted the short dhoti woven with hand-spun yarn as a mark of identification with India's rural poor. He began to live in a self-sufficient residential community, to eat simple food, and undertake long fasts as a means of both introspection and political protest. Bringing anti-colonial nationalism to the common Indians, Gandhi led them in challenging the British-imposed salt tax with the 400 km (250 mi) Dandi Salt March in 1930 and in calling for the British to quit India in 1942. He was imprisoned many times and for many years in both South Africa and India.
+
+Gandhi's vision of an independent India based on religious pluralism was challenged in the early 1940s by a Muslim nationalism which demanded a separate homeland for Muslims within British India. In August 1947, Britain granted independence, but the British Indian Empire was partitioned into two dominions, a Hindu-majority India and a Muslim-majority Pakistan. As many displaced Hindus, Muslims, and Sikhs made their way to their new lands, religious violence broke out, especially in Punjab and Bengal. Abstaining from the official celebration of independence, Gandhi visited the affected areas, attempting to alleviate distress. In the months following, he undertook several hunger strikes to stop the religious violence. The last of these was begun in Delhi on 12 January 1948, when Gandhi was 78. The belief that Gandhi had been too resolute in his defence of both Pakistan and Indian Muslims spread among some Hindus in India. Among these was Nathuram Godse, a militant Hindu nationalist from Pune, western India, who assassinated Gandhi by firing three bullets into his chest at an interfaith prayer meeting in Delhi on 30 January 1948.
+
+Gandhi's birthday, 2 October, is commemorated in India as Gandhi Jayanti, a national holiday, and worldwide as the International Day of Nonviolence. Gandhi is considered to be the Father of the Nation in post-colonial India. During India's nationalist movement and in several decades immediately after, he was also commonly called Bapu, an endearment roughly meaning "father".
+    """
+    summary_template = """
+    Given the imformation {information} about the person, I want you to create a short summary and 4 interesting facts about them.
+    Return the result in the following JSON format:
+    {{
+        "summary": "...",
+        "facts": [
+            "...",
+            "...",
+            "...",
+            "..."
+        ]
+    }}"""
+
+    summary_prompt_template = PromptTemplate(
+        input_variables=["information"],template=summary_template
+    )
+
+    llm = ChatOllama(
+        model="gpt-oss:latest",
+        temperature=0,
+    )
+
+    summary_chain = summary_prompt_template | llm
+
+    summary = summary_chain.invoke({"information": information})
+    print(summary.content)
 
 
 if __name__ == "__main__":
